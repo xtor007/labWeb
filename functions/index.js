722b1a942ec3b480902b59ee0347f0ea4eaf8591
Anatoliy <tolxpams@gmail.com>
2022-01-01 17:30:22 +0200

    edit listen

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const mailer = require('./mail')
 
 const ev = express()
 
-const PORT = process.env.PORT || '8080'
+const PORT = process.env.PORT||'8080'
 
 ev.use(bodyParser.urlencoded({ extended: false }))
 
@@ -39,12 +39,11 @@ ev.get('/form',(req,res) => {
   const path = require("path")
   res.sendFile(path.resolve('public/index.html'))
 })
-// ev.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html')
-// })
+
 ev.get('/main.css', function(req, res) {
   res.sendFile(__dirname + '../public/main.css')
 })
 
-ev.listen(PORT, () => console.log(`http://localhost:${PORT}/form`))
-//ev.listen
+ev.listen(PORT, function(){
+  console.log(`http://localhost:${PORT}/form`)
+})
