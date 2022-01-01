ff8cfd3c2119fe43334b5f9d3111d8dad3cf855c
Anatoliy <tolxpams@gmail.com>
2022-01-01 21:01:29 +0200

    edit port

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const mailer = require('./mail')
 const ev = express()
 
 //const PORT = process.env.PORT||'8080'
-const PORT = process.env.YOUR_PORT || 3000
+const PORT = process.env.YOUR_PORT || 9000
 
 ev.use(bodyParser.urlencoded({ extended: false }))
 
